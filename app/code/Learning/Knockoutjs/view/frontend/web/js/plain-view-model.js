932d9c6816a91734977001171a362c97e0f6a2a2
jeevi-27 <jeevitha.rameshbabu@ziffity.com>
2023-06-14 14:34:48 +0530

    subscribing to changes -Learning\Knockout js

--- app/code/Learning/Knockoutjs/view/frontend/web/js/plain-view-model.js
+++ app/code/Learning/Knockoutjs/view/frontend/web/js/plain-view-model.js
@@ -2,8 +2,17 @@ define(['ko'],function(ko){
     'use strict';
 
     return function(){
+        const title =ko.observable("This is title ssadf");
+        title.subscribe(function(newValue)
+        {
+                console.log('New title:',newValue);
+        });
+        title.subscribe(function(oldValue)
+        {
+                console.log('Old value:',oldValue);
+        }, this, 'beforeChange');
         return {
-            title :ko.observable("This is title ssadf")
+            title :title
         }
     }
 
